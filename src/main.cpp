16a527b053b61605ceaa66082e50ca971b5abb59
scottc11 <scott.m.campbell11@gmail.com>
2022-01-26 09:09:05 -0500

    stable playground main

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
+#include <stdbool.h>
+#include <stdint.h>
 #include "utils.h"
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    // Print results to console
+
+    int output1 = 1234;
+    int output2 = 5678;
+
     cout << endl;
-    cout << 1234 << " and some text." << endl;
-    cout << 5678 << " and some more text." << endl;
+    cout << "Output 1: " << output1 << endl;
+    cout << "Output 2: " << output2 << endl;
     cout << endl;
 
     return 0;
