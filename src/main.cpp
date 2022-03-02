4eb0c218d1501a0d70025cc9318fd8160633a460
scottc11 <scott.m.campbell11@gmail.com>
2022-03-02 16:18:30 -0500

    added bitset

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <bitset>
 #include <stdbool.h>
 #include <stdint.h>
 #include "utils.h"
@@ -10,10 +11,12 @@ int main(int argc, char *argv[])
 
     int output1 = 1234;
     int output2 = 5678;
+    bitset<8> output3 = output1;
 
     cout << endl;
     cout << "Output 1: " << output1 << endl;
     cout << "Output 2: " << output2 << endl;
+    cout << "Output 3: " << output3 << endl;
     cout << endl;
 
     return 0;
